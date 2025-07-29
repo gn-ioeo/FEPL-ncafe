0beece4d6f7734d8c520d2ad2e439cfe1b9bcd6b
gn-ioeo <thdrkdms1030@hanyang.ac.kr>
2025-07-29 11:41:17 +0900

    07.29 login

--- app/(anon)/login/page.tsx
+++ app/(anon)/login/page.tsx
@@ -1,45 +1,64 @@
+"use client";
 import Link from "next/link";
-import React from "react";
 import styles from "./page.module.scss";
+import { useState } from "react";
 
 const {
-	["login-form-box"]: loginFormBox,
-	["login-form"]: loginForm,
-	["btn-google-login"]: btnGoogleLogin,
-	["link-box"]: linkBox,
+  ["login-form-box"]: loginFormBox,
+  ["login-form"]: loginForm,
+  ["btn-google-login"]: btnGoogleLogin,
+  ["link-box"]: linkBox,
 } = styles;
 
 export default function LoginPage() {
-	return (
-		<main>
-			<div className={loginFormBox}>
-				<section className={loginForm}>
-					<h1>Rland Login</h1>
-					<form>
-						<div className="input-box">
-							<label>ID</label>
-							<input type="text" name="username" placeholder="아이디" value="demo" readOnly />
-						</div>
-						<div className="input-box">
-							<label>Password</label>
-							<input type="password" name="password" placeholder="비밀번호" value="password" readOnly />
-						</div>
-						<div>
-							<button type="button">Login</button>
-						</div>
-						<div>또는</div>
-						<div>
-							<Link href="/" className={`${btnGoogleLogin} n-icon n-icon:google_logo n-deco`}>
-								구글로 로그인
-							</Link>
-						</div>
-						<div className={linkBox}>
-							<Link href="signup">회원가입</Link>
-							<Link href="">비밀번호 찾기</Link>
-						</div>
-					</form>
-				</section>
-			</div>
-		</main>
-	);
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  return (
+    <main>
+      <div className={loginFormBox}>
+        <section className={loginForm}>
+          <h1>Rland Login</h1>
+          <form>
+            <div className="input-box">
+              <label>ID</label>
+              <input
+                type="text"
+                name="username"
+                placeholder="아이디"
+                value={username}
+                onChange={(e) => setUsername(e.target.value)}
+              />
+            </div>
+            <div className="input-box">
+              <label>Password</label>
+              <input
+                type="password"
+                name="password"
+                placeholder="비밀번호"
+                value={password}
+                onChange={(e) => setPassword(e.target.value)}
+              />
+            </div>
+            <div>
+              <button type="button">Login</button>
+            </div>
+            <div>또는</div>
+            <div>
+              <Link
+                href="/"
+                className={`${btnGoogleLogin} n-icon n-icon:google_logo n-deco`}
+              >
+                구글로 로그인
+              </Link>
+            </div>
+            <div className={linkBox}>
+              <Link href="signup">회원가입</Link>
+              <Link href="">비밀번호 찾기</Link>
+            </div>
+          </form>
+        </section>
+      </div>
+    </main>
+  );
 }
